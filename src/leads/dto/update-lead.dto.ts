26cafb578e86bf207e3b037a8e1cb867beec3397
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:59 +0500

    fix: memory leak muammosi hal qilindi

--- src/leads/dto/update-lead.dto.ts
+++ src/leads/dto/update-lead.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// error handling yaxshilandi
 // bundle size optimallashtirildi
 // prettier formatlash
 import { CreateLeadDto } from './create-lead.dto';
