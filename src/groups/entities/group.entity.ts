76a8bf3d52a71d7a4b26873f303d78c4f51d1c7a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:43 +0500

    style: ESLint qoidalariga moslashtirish

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -5,6 +5,7 @@ import { Course } from '../../courses/entities/course.entity';
 // changelog yangilandi
 // caching mexanizmi qo'shildi
 
+// type error tuzatildi
 @Entity('groups')
 export class Group {
 // middleware funksiyalari qo'shildi
