915b12b130f33caa74628f7b0ba8fc01b76b4171
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:39 +0500

    docs: API hujjatlarini qo'shish

--- src/groups/dto/update-group.dto.ts
+++ src/groups/dto/update-group.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGroupDto } from './create-group.dto';
+// kod uslubini yaxshilash
 
 export class UpdateGroupDto extends PartialType(CreateGroupDto) {}
