36010e4a5f6188069579640fab17605f30aa3bd6
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:39 +0500

    perf: caching mexanizmi qo'shildi

--- src/groups/dto/update-group.dto.ts
+++ src/groups/dto/update-group.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGroupDto } from './create-group.dto';
 // kod uslubini yaxshilash
+// kod strukturasini yaxshilash
 
 export class UpdateGroupDto extends PartialType(CreateGroupDto) {}
