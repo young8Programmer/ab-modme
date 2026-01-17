f6e19aaa2dfd3c90dee3175011c812b09b4d8247
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:03:31 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+// API endpoint testlari qo'shildi
 // routing muammosi hal qilindi
   app.useGlobalPipes(new ValidationPipe());
 // kod formatlash va indentatsiya
