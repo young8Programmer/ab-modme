a705e8c05744f0b11e74392711ad464e7b50c6e9
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:33 +0500

    chore: package.json yangilandi

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// authentication xatosi tuzatildi
 import { CreateCourseDto } from './create-course.dto';
 
 // database connection muammosi hal qilindi
