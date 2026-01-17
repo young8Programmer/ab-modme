d10ebaef1fd795d1b29d20a031cfa0ab27710053
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:58 +0500

    chore: dependencies yangilandi

--- src/leads/dto/update-lead.dto.ts
+++ src/leads/dto/update-lead.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 // bundle size optimallashtirildi
+// prettier formatlash
 import { CreateLeadDto } from './create-lead.dto';
 
 export class UpdateLeadDto extends PartialType(CreateLeadDto) {}
