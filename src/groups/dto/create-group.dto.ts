58e4cb4bdba9c230e1a2963baefe7ef74d0e9ece
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:38 +0500

    fix: authentication xatosi tuzatildi

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -3,6 +3,7 @@ import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 export class CreateGroupDto {
   @IsString()
   @IsNotEmpty()
+// componentlarni qayta tashkilash
 // middleware funksiyalari qo'shildi
 // routing muammosi hal qilindi
   @MinLength(2)
