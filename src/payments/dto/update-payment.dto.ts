4cf7f3190924c36a5bcc3019ba2173e94549e378
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:05:32 +0500

    chore: environment variables sozlandi

--- src/payments/dto/update-payment.dto.ts
+++ src/payments/dto/update-payment.dto.ts
@@ -2,4 +2,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentDto } from './create-payment.dto';
 
+// kod uslubini yaxshilash
 export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
