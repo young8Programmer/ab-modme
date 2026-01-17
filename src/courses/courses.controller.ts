c211dc5e493e81671ffb7e8b2f30724e59665522
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:23 +0500

    feat: admin dashboard yaratildi

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// database testlari qo'shildi
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
