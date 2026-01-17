39a3d8170b93ae81a3a1c58be795211c0220c775
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:12 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
+// README faylini yangilash
   let appController: AppController;
 
   beforeEach(async () => {
