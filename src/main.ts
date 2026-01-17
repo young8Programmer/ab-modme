c656ec222bb5d9e8e644b605b3d3d93dbbc38fa4
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:03:25 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// type error tuzatildi
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
