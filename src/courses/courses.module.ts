0618a43fc6f5f989dca0a80f57bc135223ecbdf0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:25 +0500

    chore: package.json yangilandi

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,6 +4,7 @@ import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Course } from './entities/course.entity';
 
+// component testlari yaratildi
 @Module({
   imports: [TypeOrmModule.forFeature([Course])],
 // caching mexanizmi qo'shildi
