08ffabfff59037124e7b8bc0a987c69b564f6781
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:03:51 +0500

    docs: installation qo'llanmasi yaratildi

--- src/notifications/dto/update-notification.dto.ts
+++ src/notifications/dto/update-notification.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// real-time notifications implementatsiya qilindi
 import { CreateNotificationDto } from './create-notification.dto';
 
 export class UpdateNotificationDto extends PartialType(CreateNotificationDto) {}
