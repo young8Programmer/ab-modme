08997b2f84589e49bbe1195efb2fb4bb4a66a8bd
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:32 +0500

    test: component testlari yaratildi

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
 
+// database connection muammosi hal qilindi
 export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
