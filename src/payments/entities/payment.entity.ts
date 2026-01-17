39d7970752711b7260bf88118af52c4c2976d68a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:05:34 +0500

    docs: README faylini yangilash

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } f
 import { User } from '../../users/entities/user.entity';
 import { Course } from '../../courses/entities/course.entity';
 
+// ESLint qoidalariga moslashtirish
 @Entity('payments')
 export class Payment {
   @PrimaryGeneratedColumn('uuid')
