3f18518b9297e1c2a7ed3cda0ecb97d924110c77
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:05:08 +0500

    feat: user authentication qo'shildi

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+// ESLint qoidalariga moslashtirish
 import { Notification } from './entities/notification.entity';
 
 @Module({
