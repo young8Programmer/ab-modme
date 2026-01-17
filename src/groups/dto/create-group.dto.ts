4d653fd9bd2b06a3f781eef0d86d216f35b0c877
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:37 +0500

    feat: user authentication qo'shildi

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -3,6 +3,7 @@ import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 export class CreateGroupDto {
   @IsString()
   @IsNotEmpty()
+// middleware funksiyalari qo'shildi
 // routing muammosi hal qilindi
   @MinLength(2)
   name: string;
