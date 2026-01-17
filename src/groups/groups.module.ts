d23c567a0423c351362e895148ec78e382cd08ff
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:48 +0500

    chore: CI/CD pipeline sozlandi

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
+// admin dashboard yaratildi
 import { Group } from './entities/group.entity';
 
 @Module({
