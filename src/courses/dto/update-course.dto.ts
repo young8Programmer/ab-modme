bbab9e59b2c9ef8378bb0043d5a65962b9fdd259
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:01:34 +0500

    feat: database migrations yaratildi

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -6,3 +6,4 @@ import { CreateCourseDto } from './create-course.dto';
 
 // database connection muammosi hal qilindi
 export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+// environment variables sozlandi
